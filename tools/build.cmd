22d04c4ef894244a5a11e4ef16e88a64da4e6dcd
Alexander Borsuk <me@alex.bio>
2019-05-31 07:51:37 +0300

    Updated build scripts to correctly use configs for different environments

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,22 +1,29 @@
 @ECHO OFF
-REM Generates static html site.
+REM Generates production version of static html site.
 REM
 REM Created by Alexander Borsuk <me@alex.bio> from Minsk, Belarus.
+REM
+REM You can always install up-to-date hugo binary from here: https://github.com/gohugoio/hugo/releases
 
 SETLOCAL
 
 SET tools_dir=%~dp0
 REM Project root directory without a slash at the end.
 SET root=%tools_dir%..
-SET bin=%root%\bin
 
-REM Try to use hugo from bin submodule and fall back to version in PATH
-IF EXIST %root%\bin\hugo.exe (
-  SET hugo=%root%\bin\hugo.exe
+WHERE hugo.exe >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+  REM Try to use hugo from bin submodule.
+  IF EXIST %root%\bin\hugo.exe (
+    SET hugo=%root%\bin\hugo.exe
+  ) ELSE (
+    ECHO "Please install hugo binary from here: https://github.com/gohugoio/hugo/releases"
+    EXIT /B 200
+  )
 ) ELSE (
   SET hugo=hugo.exe
 )
 
-%hugo% -s %root% || ECHO "Please install hugo binary from here: https://github.com/gohugoio/hugo/releases"
+%hugo% -s %root% --environment production
 
 ENDLOCAL
--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Generates static html site.
+# Generates production version of static html site.
 #
 # Created by Alexander Borsuk <me@alex.bio> from Minsk, Belarus.
 #
@@ -11,14 +11,10 @@
 # -o pipefail aborts if on any failed pipe operation.
 set -euo pipefail
 
+HUGO_BINARY=hugo
+
+command -v $HUGO_BINARY >/dev/null 2>&1 || { echo >&2 "Please install the latest hugo binary from brew (MacOS) or from here: https://github.com/gohugoio/hugo/releases"; exit 1; }
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-HUGO_BINARY=hugo
-HUGO_PARAMS="-s "$SCRIPT_DIR/.." --cleanDestinationDir=true"
-# Pass custom config file for development builds.
-if [ -n "${HUGO_CONFIG+set}" ]; then
-  HUGO_PARAMS="$HUGO_PARAMS --config=$HUGO_CONFIG"
-fi
-
-# Build whole site.
-"$HUGO_BINARY" $HUGO_PARAMS || { echo "Please install hugo binary from here: https://github.com/gohugoio/hugo/releases"; exit 1; }
+$HUGO_BINARY -s "$SCRIPT_DIR/.." --cleanDestinationDir=true --environment production
