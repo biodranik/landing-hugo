63d7c9449e826aa7a6a069fdb7720ca4bb068ddc
Alexander Borsuk <me@alex.bio>
2019-05-31 07:51:37 +0300

    Updated build scripts to correctly use configs for different environments

--- tools/build_dev.sh
+++ tools/build_dev.sh
@@ -11,31 +11,14 @@
 # -o pipefail aborts if on any failed pipe operation.
 set -euo pipefail
 
+HUGO_BINARY=hugo
+
+command -v $HUGO_BINARY >/dev/null 2>&1 || { echo >&2 "Please install the latest hugo binary from brew (MacOS) or from here: https://github.com/gohugoio/hugo/releases"; exit 1; }
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "****************************************************"
 echo "****** Building dev version of the site(s)... ******"
 echo "****************************************************"
 
-# Create a copy of config in a temporary directory and modify it for the dev build.
-TMP_FILE=`mktemp`
-DEV_CONFIG="${TMP_FILE}.yml"
-cp "$SCRIPT_DIR/../config.yaml" "$DEV_CONFIG"
-rm "$TMP_FILE"
-# config.yaml production values and their dev replacements for sed:
-declare -a toReplace=(
-  's|https://www.vibrobox.com/|https://vibrobox.github.io/preview.com/|g'
-  's|https://www.vibrobox.ru/|https://vibrobox.github.io/preview.ru/|g'
-)
-for sed_param in "${toReplace[@]}"; do
-  sed -i.bak "$sed_param" "$DEV_CONFIG"
-done
-
-# Build site with modified dev config.
-HUGO_CONFIG="$DEV_CONFIG" source "$SCRIPT_DIR/build.sh"
-
-# Remove production CNAME for dev builds.
-rm "$SCRIPT_DIR/../public/en/CNAME" "$SCRIPT_DIR/../public/ru/CNAME"
-
-# Remove temporary dev config.
-rm "$DEV_CONFIG"
+$HUGO_BINARY -s "$SCRIPT_DIR/.." --cleanDestinationDir=true --environment development
--- tools/watch.cmd
+++ tools/watch.cmd
@@ -6,16 +6,22 @@ REM Created by Alexander Borsuk <me@alex.bio> from Minsk, Belarus.
 SETLOCAL
 
 SET tools_dir=%~dp0
+REM Project root directory without a slash at the end.
 SET root=%tools_dir%..
 
-REM Try to use hugo from bin submodule and fall back to version in PATH
-IF EXIST %root%\bin\hugo.exe (
-  SET hugo=%root%\bin\hugo.exe
+WHERE hugo.exe >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+  REM Try to use hugo from bin submodule.
+  IF EXIST %root%\bin\hugo.exe (
+    SET hugo=%root%\bin\hugo.exe
+  ) ELSE (
+    ECHO "Please install the latest extended hugo binary from here: https://github.com/gohugoio/hugo/releases"
+    EXIT /B 200
+  )
 ) ELSE (
   SET hugo=hugo.exe
 )
 
-REM Hugo (extended) automatically rebuilds the site on any scss changes.
-%hugo% server -s %root% || ECHO "Please install hugo binary from here: https://github.com/gohugoio/hugo/releases"
+%hugo% server -s %root% --environment development
 
 ENDLOCAL
